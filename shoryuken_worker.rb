12281418d27f639c109ab5e71c9446a3a41cd3b4
YumaInaura <yuma.inaura@gmail.com>
2019-09-18 09:24:44 +0900

    ShoryukenWorker

--- shoryuken_worker.rb
+++ shoryuken_worker.rb
@@ -1,9 +1,13 @@
 class ShoryukenWorker
   include Shoryuken::Worker
 
-  shoryuken_options queue: 'example1', auto_delete: true
-  shoryuken_options queue: 'example2', auto_delete: true
-  shoryuken_options queue: 'example3', auto_delete: true
+  if ENV['SHORYUKEN_QUEUES']
+    shoryuken_options queue: queue, auto_delete: true
+  else
+    shoryuken_options queue: 'example1', auto_delete: true
+    shoryuken_options queue: 'example2', auto_delete: true
+    shoryuken_options queue: 'example3', auto_delete: true
+  end
 
   def perform(sqs_msg, name)
     puts '-' * 100
